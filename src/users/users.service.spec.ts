baebbce9d7d9e9e127ddd46ff886c82de73cfdc5
DURAND Malo <malo.durand@epitech.eu>
2024-09-13 14:16:44 +0200

    tests: adding more unit tests for UsersService.

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -19,6 +19,35 @@ function randomPassword() {
   return faker.internet.password({ length: 16, memorable: false });
 }
 
+function getGdprCompliantUser(user: User) {
+  return {
+    firstname: user.firstname,
+    lastname: user.lastname,
+    created_at: user.created_at,
+  };
+}
+
+function getUserShort(user: User) {
+  return {
+    email: user.email,
+    firstname: user.firstname,
+    lastname: user.lastname,
+    last_connection: user.last_connection,
+    created_at: user.created_at,
+  };
+}
+
+function getUserForAuthentication(user: User) {
+  return {
+    id: user.id,
+    email: user.id,
+    firstname: user.firstname,
+    lastname: user.lastname,
+    hashed_password: user.hashed_password,
+    deleted_at: user.deleted_at,
+  };
+}
+
 async function makeFakeUser(
   isAdmin: boolean = false,
   hasLastConnection: boolean = false,
@@ -83,7 +112,7 @@ describe('UsersService', () => {
     expect(usersService).toBeDefined();
   });
 
-  describe('getAll', () => {
+  describe('findMany', () => {
     it('should return the list of users', async () => {
       const dbUsers = await makeDbUsers();
       const filtered = dbUsers.filter((user) => null === user.deleted_at);
@@ -94,9 +123,155 @@ describe('UsersService', () => {
       }));
       prismaService.users.findMany.mockResolvedValueOnce(expected as any);
 
-
       const users = await usersService.findMany();
       expect(users).toStrictEqual(expected);
+      expect(prismaService.users.findMany).toHaveBeenCalledWith({
+        where: { deleted_at: null },
+        select: {
+          id: true,
+          firstname: true,
+          lastname: true,
+        },
+      });
+    });
+  });
+
+  describe('findUnique', () => {
+    it("should return the user by it's ID", async () => {
+      const gdprCompliance = false;
+      const dbUsers = await makeDbUsers();
+      const filtered = dbUsers[0];
+      const expected = [filtered].map(
+        !gdprCompliance ? getUserShort : getGdprCompliantUser,
+      )[0];
+      prismaService.users.findUnique.mockResolvedValueOnce(expected as any);
+
+      const user = await usersService.findUnique(filtered.id, gdprCompliance);
+      expect(user).toStrictEqual(expected);
+      expect(prismaService.users.findUnique).toHaveBeenCalledWith({
+        where: { id: filtered.id, deleted_at: null },
+        select: {
+          email: !gdprCompliance,
+          firstname: true,
+          lastname: true,
+          last_connection: !gdprCompliance,
+          created_at: true,
+        },
+      });
+    });
+
+    it('should return null for deleted users', async () => {
+      const dbUsers = await makeDbUsers();
+      const filtered = dbUsers.find((user) => null !== user.deleted_at);
+      const expected = null;
+      prismaService.users.findUnique.mockResolvedValueOnce(expected as any);
+
+      const user = await usersService.findUnique(filtered.id, false);
+      expect(user).toStrictEqual(expected);
+      expect(prismaService.users.findUnique).toHaveBeenCalledWith({
+        where: { id: filtered.id, deleted_at: null },
+        select: {
+          email: true,
+          firstname: true,
+          lastname: true,
+          last_connection: true,
+          created_at: true,
+        },
+      });
+    });
+  });
+
+  describe("find user for authentication by it's ID", () => {
+    it("should return the user by it's ID", async () => {
+      const dbUsers = await makeDbUsers();
+      const filtered = dbUsers[0];
+      const expected = [filtered].map(getUserForAuthentication)[0];
+      prismaService.users.findUnique.mockResolvedValueOnce(expected as any);
+
+      const user = await usersService.findUniqueByIDForAuthentication(
+        filtered.id,
+      );
+      expect(user).toStrictEqual(expected);
+      expect(prismaService.users.findUnique).toHaveBeenCalledWith({
+        where: { id: filtered.id, deleted_at: null },
+        select: {
+          id: true,
+          email: true,
+          firstname: true,
+          lastname: true,
+          hashed_password: true,
+          deleted_at: true,
+        },
+      });
+    });
+
+    it('should return null for deleted users', async () => {
+      const dbUsers = await makeDbUsers();
+      const filtered = dbUsers.find((user) => null !== user.deleted_at);
+      const expected = null;
+      prismaService.users.findUnique.mockResolvedValueOnce(expected as any);
+      const user = await usersService.findUniqueByIDForAuthentication(
+        filtered.id,
+      );
+      expect(user).toStrictEqual(expected);
+      expect(prismaService.users.findUnique).toHaveBeenCalledWith({
+        where: { id: filtered.id, deleted_at: null },
+        select: {
+          id: true,
+          email: true,
+          firstname: true,
+          lastname: true,
+          hashed_password: true,
+          deleted_at: true,
+        },
+      });
+    });
+  });
+
+  describe("find user for authentication by it's email", () => {
+    it("should return the user by it's email", async () => {
+      const dbUsers = await makeDbUsers();
+      const filtered = dbUsers[0];
+      const expected = [filtered].map(getUserForAuthentication)[0];
+      prismaService.users.findUnique.mockResolvedValueOnce(expected as any);
+
+      const user = await usersService.findUniqueByEmailForAuthentication(
+        filtered.email,
+      );
+      expect(user).toStrictEqual(expected);
+      expect(prismaService.users.findUnique).toHaveBeenCalledWith({
+        where: { email: filtered.email, deleted_at: null },
+        select: {
+          id: true,
+          email: true,
+          firstname: true,
+          lastname: true,
+          hashed_password: true,
+          deleted_at: true,
+        },
+      });
+    });
+
+    it('should return null for deleted users', async () => {
+      const dbUsers = await makeDbUsers();
+      const filtered = dbUsers.find((user) => null !== user.deleted_at);
+      const expected = null;
+      prismaService.users.findUnique.mockResolvedValueOnce(expected as any);
+      const user = await usersService.findUniqueByEmailForAuthentication(
+        filtered.email,
+      );
+      expect(user).toStrictEqual(expected);
+      expect(prismaService.users.findUnique).toHaveBeenCalledWith({
+        where: { email: filtered.email, deleted_at: null },
+        select: {
+          id: true,
+          email: true,
+          firstname: true,
+          lastname: true,
+          hashed_password: true,
+          deleted_at: true,
+        },
+      });
     });
   });
 });
